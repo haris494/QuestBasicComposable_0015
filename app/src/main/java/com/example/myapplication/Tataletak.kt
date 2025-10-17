c9079da8205a3aab60ab6626fc8c95b102836dae
haris49 <hariis.459@gmail.com>
2025-10-17 10:09:36 +0700

    first commit

---
+++ app/src/main/java/com/example/myapplication/Tataletak.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import java.lang.reflect.Modifier
+
+@Composable
+fun TataletakColumn(modifier: Modifier) {
+    Column(modifier = modifier.padding(top = 20.dp,start = 20.dp, end = 20.dp)) {
+}
